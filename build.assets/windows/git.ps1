1038fd0865f1aea2927b56648aed234a750f9cfd
Trent Clarke <trent@goteleport.com>
2022-07-26 15:43:33 +1000

    ps1 formatting

--- build.assets/windows/git.ps1
+++ build.assets/windows/git.ps1
@@ -12,10 +12,10 @@ function Enable-Git {
             $SSHDir = "$Workspace/.ssh"
             New-Item -Path "$SSHDir" -ItemType Directory | Out-Null
             $PrivateKey | Out-File -Encoding ascii "$SSHDir/id_rsa"
-            Invoke-WebRequest "https://api.github.com/meta" -UseBasicParsing 
-                | ConvertFrom-JSON 
-                | Select-Object -ExpandProperty "ssh_keys" 
-                | ForEach-Object {"github.com $_"} 
+            Invoke-WebRequest "https://api.github.com/meta" -UseBasicParsing `
+                | ConvertFrom-JSON `
+                | Select-Object -ExpandProperty "ssh_keys" `
+                | ForEach-Object {"github.com $_"} `
                 | Out-File -Encoding ASCII "$SSHDir/known_hosts"`
             $SSHCmd = "ssh -i $SSHDir/id_rsa -o UserKnownHostsFile=$SSHDir/known_hosts -F/dev/null"
             $Env:GIT_SSH_COMMAND = $SSHDir
